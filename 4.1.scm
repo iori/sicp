a82be1cc2b55ccc3d6d7d09889526c41e99c981e
iori <iori.nishizawa@gmail.com>
2023-01-10 07:47:54 +0900

    feat: ex-4.2

--- 4.1.scm
+++ 4.1.scm
@@ -2,16 +2,16 @@
 (load "eval-apply.scm")
 
 ; ex-4.1
-(define env (setup-environment))
+(define genv (setup-environment))
+(define expression '((set! val (+ val 2)) (set! val (* val 2))))
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
     '()
     (cons (eval (first-operand exps) env)
           (list-of-values (rest-operands exps) env))))
-(define-variable! 'val 10 env)
-(define expression '((set! val (+ val 2)) (set! val (* val 2))))
-(print (list-of-values expression env))
+(define-variable! 'val 10 genv)
+(print (list-of-values expression genv))
 
 ;; 被演算子を左から右へ評価する list-of-values
 (define (list-of-values exps env)
@@ -20,9 +20,8 @@
     (let ((first-eval (eval (first-operand exps) env)))
       (cons first-eval
             (list-of-values (rest-operands exps) env)))))
-(define-variable! 'val 10 env)
-(define expression '((set! val (+ val 2)) (set! val (* val 2))))
-(print (list-of-values expression env))
+(define-variable! 'val 10 genv)
+(print (list-of-values expression genv))
 
 ;; 被演算子を右から左へ評価する list-of-values
 (define (list-of-values exps env)
@@ -32,7 +31,65 @@
       (cons (eval (first-operand exps) env)
             first-eval))))
 
+(define-variable! 'val 10 genv)
+(print (list-of-values expression genv))
 
-(define-variable! 'val 10 env)
-(define expression '((set! val (+ val 2)) (set! val (* val 2))))
-(print (list-of-values expression env))
+; ex-4.2
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ; 手続き作用を代入の前にする
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        (else
+          (error "Unknown expression type - - EVAL" exp))))
+
+(define expression '(set! val (+ 1 2)))
+; a. application?がassigmentに到達させない
+;
+;    gosh$ (eval expression genv)
+;    *** ERROR: Unbound variable set!
+
+; b.
+(define (application? exp)
+  (tagged-list? exp 'call))
+
+(define (eval exp env)
+  ; 手続き作用を最初にする
+  (cond ((application? exp)
+         (let ((remove-call-exp (cdr exp)))
+           (apply (eval (operator remove-call-exp) env)
+                  (list-of-values (operands remove-call-exp) env))))
+        ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        (else
+          (error "Unknown expression type - - EVAL" exp))))
+
+(define expression '(call + 1 2))
+; (eval expression genv)
+; 3
+
+(load "eval-apply.scm")
