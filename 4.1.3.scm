9616974edb6a3ac51cbe5941c2173d8cf601ed25
iori <iori.nishizawa@gmail.com>
2023-02-10 07:19:26 +0900

    feat: ex-4.21

--- 4.1.3.scm
+++ 4.1.3.scm
@@ -171,3 +171,35 @@
 
 (scan-out-defines body)
 
+; ex-4.21
+((lambda (n)
+   ((lambda (fact)
+      (fact fact n))
+    (lambda (ft k)
+      (if (= k 1)
+        1
+        (* k (ft ft (- k 1)))))))
+ 10)
+
+; a
+((lambda (n)
+   ((lambda (fib)
+      (fib fib n))
+    (lambda (fb k)
+      (cond ((= k 0) 0)
+            ((= k 1) 1)
+            (else (+ (fb fb (- k 1))
+                     (fb fb (- k 2))))))))
+ 10)
+
+; b
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     ;(if (= n 0) #t (od? ?? ?? ??)))
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     ;(if (= n 0) #f (ev? ?? ?? ??)))))
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
+
